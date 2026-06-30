5497af24c738e70a565d36da4934939ee761d9ac
Amit Trivedi <amit2820@yahoo.co.in>
2026-06-30 10:21:45 +0530

    Feature branch: added missing index group stats query

--- performance_tuning/missing_indexes.sql
+++ performance_tuning/missing_indexes.sql
@@ -1,3 +1,6 @@
 select 1; -- in master branch
 -- Added by MAIN branch
 SELECT * FROM sys.dm_db_missing_index_details; -- Added by MAIN branch
+
+-- Added by FEATURE branch
+SELECT * FROM sys.dm_db_missing_index_group_stats;
